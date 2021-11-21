fee2b7bb5d6b448c18bae7b2ca98f749699f065f
DESKTOP-FH5BK1A\chsan <ch.sandeepreddy544@gmail.com>
2021-11-21 15:34:05 -0600

    git commit

--- Validations/src/app/app.component.ts
+++ Validations/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, FormArray, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +17,10 @@ export class AppComponent implements OnInit{
   {
     return this.applicationForm.get('email1') as FormControl;
   }
+  get phone1(): FormControl
+  {
+    return this.applicationForm.get('phone1') as FormControl;
+  }
 
   constructor(private fb: FormBuilder)
   {
@@ -33,9 +37,10 @@ export class AppComponent implements OnInit{
 
       nameControl: ['', [Validators.required, Validators.minLength(2)]],
       email1:['', [Validators.required, Validators.email]],
+      phone1:['', [Validators.required, phoneValidator]],
       contactInfo: this.fb.group({
         email:'sandee@gmail.com',
-        phone: '999'
+        phone:'999'
       }),
       hobby:'',
       over18:''
@@ -49,4 +54,14 @@ export class AppComponent implements OnInit{
     console.log(this.applicationForm);
   }
   
+}
+function phoneValidator(control: AbstractControl): {[key:string]: any} | null{
+  const phonRegex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+  if(phonRegex.test(control.value)){
+    return null;
+  }
+  else
+  return{
+    invalidPhone: true
+  };
 }
